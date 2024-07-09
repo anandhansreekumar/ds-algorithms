77dd82a97443ed0c3a4a59ebf481b79ef4b73f72
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-07-09 18:48:03 +0530

    feat: linked list append, prepend

---
+++ 003_linked_lists/01_basics.js
@@ -0,0 +1,50 @@
+// 10 -> 5 -> 16
+// let linkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null,
+//       },
+//     },
+//   },
+// };
+
+class LinkedList {
+  constructor(value) {
+    this.head = this.createNode(value, null);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  createNode(value, nextNode) {
+    return {
+      value,
+      next: nextNode,
+    };
+  }
+
+  append(value) {
+    const newNode = this.createNode(value, null);
+    this.tail = newNode;
+    this.head.next = this.tail;
+    this.length++;
+
+    console.log(this);
+  }
+
+  prepend(value) {
+    const newNode = this.createNode(value, this.head);
+    this.head = newNode;
+    this.length++;
+
+    console.log(this);
+  }
+}
+
+const linkedList = new LinkedList(10);
+linkedList.append(5);
+linkedList.append(16);
+linkedList.prepend(1000);
