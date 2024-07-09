e08033869af99179a9790f5cd76856a9e2d407ec
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-07-09 16:21:57 +0530

    feat: hash table keys

--- 002_hash_tables/01_custom.js
+++ 002_hash_tables/01_custom.js
@@ -14,23 +14,65 @@ class HashTable {
   }
 
   get(key) {
-    const hashedIndex = this._hash(key);
+    // O(1)
+    // or
+    // O(n) - collisions
+    const hashedAddress = this._hash(key);
 
-    return this.data[hashedIndex];
+    const currentBucket = this.data[hashedAddress];
+
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        const [itemKey, itemValue] = currentBucket[i];
+        if (itemKey === key) {
+          return itemValue;
+        }
+      }
+    }
+
+    return undefined;
   }
 
   set(key, value) {
-    const hashedIndex = this._hash(key);
+    // O(1)
+    const hashedAddress = this._hash(key);
 
-    if (!this.data[hashedIndex]) {
-      this.data[hashedIndex] = [];
-      this.data[hashedIndex].push([key, value]);
+    if (!this.data[hashedAddress]) {
+      this.data[hashedAddress] = [];
     }
+
+    this.data[hashedAddress].push([key, value]);
+
+    return this.data;
+  }
+
+  keys() {
+    // O(n)
+    // or
+    // O(n^2) - collisions
+    const tableKeys = [];
+
+    for (let i = 0; i < this.data.length; i++) {
+      const currentBucket = this.data[i];
+
+      if (currentBucket) {
+        for (let j = 0; j < currentBucket.length; j++) {
+          const [itemKey] = currentBucket[j];
+          tableKeys.push(itemKey);
+        }
+      }
+    }
+
+    return tableKeys;
   }
 }
 
 const hashTable = new HashTable(50);
 
-hashTable.set("grapes", 1000);
+hashTable.set("grapes", 100);
+hashTable.set("apples", 30);
+hashTable.set("oranges", 70);
 
 console.log(hashTable.get("grapes"));
+
+console.log(hashTable.keys());
