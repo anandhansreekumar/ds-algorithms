ac67ab96ca07ff786cf6595e027e6475f8299595
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-07-11 17:38:39 +0530

    fix: linked list append

---
+++ 003_linked_lists/01_singly_linked_list.js
@@ -0,0 +1,112 @@
+class ListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  listAllNodes() {
+    const items = [];
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      items.push(currentNode.data);
+      currentNode = currentNode.next;
+    }
+
+    return items;
+  }
+
+  prependNode(data) {
+    const newNode = new ListNode(data);
+
+    if (this.length) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+
+    return this;
+  }
+
+  appendNode(data) {
+    const newNode = new ListNode(data);
+
+    if (this.length) {
+      this.tail.next = newNode;
+    } else {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+
+    return this;
+  }
+
+  findNodeByIndex(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+
+    let i = 0;
+    let currentNode = this.head;
+
+    while (i < index) {
+      currentNode = currentNode.next;
+      i++;
+    }
+
+    return currentNode;
+  }
+
+  addNodeAtIndex(index, value) {
+    // Invalid
+    if (index < 0 || index >= this.length) {
+      return;
+    }
+
+    // Head
+    if (index === 0) {
+      return prependNode(value);
+    }
+
+    // Tail
+    if (index === this.length - 1) {
+      return this.appendNode(value);
+    }
+
+    const newNode = new ListNode(value);
+
+    const leader = this.findNodeByIndex(index - 1);
+    const holdingPointer = leader.next;
+    newNode.next = holdingPointer;
+    leader.next = newNode;
+  }
+
+  length() {
+    return this.length;
+  }
+
+  clear() {
+    this.head = null;
+  }
+}
+
+const ll = new LinkedList();
+
+ll.prependNode(1);
+ll.prependNode(2);
+ll.prependNode(3);
